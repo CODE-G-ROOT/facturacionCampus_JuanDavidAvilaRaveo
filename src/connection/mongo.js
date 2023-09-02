58403944029339b520203aaad5c4db79995705c0
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-09-01 22:27:03 -0500

    ci: :art: estructura de archivos
    asdlfkj

---
+++ src/connection/mongo.js
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+
+let conn = undefined;
+
+const connectString = 'mongodb+srv://JuanDev856:juan856@juandev856.ikw3dq6.mongodb.net/';
+const cliente = new MongoClient(connectString);
+
+try {
+    conn = await cliente.connect();
+} catch (error) {
+    console.error(error);
+}
+
+let db = conn.db('db_campus_alquiler')
+
+export default db;
---
+++ src/routers/app.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { confiGET } from "../limit/limit.js";
+import { middle_Verify_Alquiler, DTO_data_alquiler } from "../middleware/alquiler.js";
+import { 
+    getAlquileres,
+    getAlquileres_cliente,
+    getAlquileres_costo_total,
+    getAlquileres_fecha,
+    getAlquileres_fechas,
+    getAlquileres_id,
+    getAlquileres_actives, 
+    postAlquileres,
+    postManyAlquileres,
+    putAlquiler
+} from "../controllers/alquiler.js";
+
+
+const alquiler = Router();
+
+alquiler.get("/", confiGET(), getAlquileres, middle_Verify_Alquiler);
+alquiler.get("/search=:id", confiGET(), getAlquileres_id, middle_Verify_Alquiler);
+alquiler.get("/search-estado=:sts", confiGET(), getAlquileres_actives, middle_Verify_Alquiler);
+alquiler.get("/search-costo_total=:id", confiGET(), getAlquileres_costo_total, middle_Verify_Alquiler);
+alquiler.get("/search-start_date=:inicio&end_date=:fin", confiGET(), getAlquileres_fechas, middle_Verify_Alquiler);
+alquiler.get("/search-fechas=Punto_12", confiGET(), getAlquileres_fecha, middle_Verify_Alquiler);
+alquiler.get("/search-cliente", confiGET(), getAlquileres_cliente, middle_Verify_Alquiler);
+
+alquiler.post("/", confiGET(), postAlquileres, DTO_data_alquiler);
+alquiler.post("/many", confiGET(), postManyAlquileres, DTO_data_alquiler);
+
+alquiler.put("/:id", confiGET(), putAlquiler, DTO_data_alquiler);
+alquiler.put("/many", confiGET(), postManyAlquileres, DTO_data_alquiler);
+
+export default alquiler;
